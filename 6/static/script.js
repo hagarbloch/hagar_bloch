80dab1d84a824c4650a94914ff6887eaf0b43441
hagar_bloch <hagarbl@post.bgu.ac.il>
2023-05-14 17:31:52 +0300

    exercise 6

--- 6/static/script.js
+++ 6/static/script.js
@@ -1,66 +1,14 @@
-var x = 5;
-var y = 4;
-let z = 2;
+const button = document.getElementById('flip-button');
+let rotation = 0;
 
-console.log(x+y);
-
-var d = new Date()
-console.log(d);
-var y= d.getFullYear();
-console.log(y);
-
-var Alon = {
-    age : 21,
-    Family : "Levy",
-    Time : new Date() 
-};
-console.log(Alon.Time);
-var h = d.getHours();
-console.log(h);
-
-if (h < 6) {
-    document.getElementById("greetingh1").innerHTML = "ZZZZ";
-
-} 
-else if (h<12) {
-    document.getElementById("greetingh1").innerHTML = "Good Morning";
-    document.getElementsByTagName("body").classList.remove("NightStyle");
-
-
-}
-else if (h<16) {
-    document.getElementById("greetingh1").innerHTML = "Good Afternoon";
-
-}
-else if (h<20) {
-    document.getElementById("greetingh1").innerHTML = "Good Evening";
-    document.getElementById("body").classList.add("NightStyle");
-    
-
-}
-else {
-    document.getElementById("greetingh1").innerHTML = "Good Night";
-
-};
-
-function Timefunc() {
-    document.getElementById("Time").innerHTML = d.getFullYear();
-    document.getElementById("btn").innerHTML = "that's it";
-    
+function flipButton() {
+  rotation = rotation + 90;
+  if (rotation === 360) {
+    button.classList = 'rotate-360';
+  } else {
+    button.classList = `rotate-${rotation % 360}`;
+  }
+  button.style.transform = `rotate(${rotation}deg)`;
 }
 
-var cars = ["volvo", "mazda", "ferrari", "mazarati", "lamburgini"];
-var l = cars.length;
-console.log(l);
-console.log(cars[4]);
-
-var text = "";
-
-
-for (let i =0; cars< cars.length; i++) {
-    text += cars[i] +"<br>";
-    document.getElementById("Time").innerHTML = text;
-    console.log(text)
-
-}
-
+button.addEventListener('click', flipButton);
